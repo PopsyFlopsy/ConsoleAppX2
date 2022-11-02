8928e595918f44e258910298b4aa189b90a51fbb
Frosty <Frosty@DESKTOP-GS2LG2N>
2022-11-02 19:41:42 +0300

    Add project files.

---
+++ ConsoleAppX2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleAppX2
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = 0.75;
+            Guy player = new Guy() { Cash = 100, Name = "The player"};
+            Console.WriteLine($"Hello, beetch, odds are {odds}");
+            for (int i = 0; player.Cash > 0; i++)
+            {
+                Console.WriteLine($"{player.Name} has {player.Cash}");
+                Console.Write("How much do you want to bet: ");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = amount * 2;
+                    if (random.NextDouble() > odds)
+                    {
+                        Console.WriteLine($"You win {pot}, slut");
+                        player.Cash += pot;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ya los beetc");
+                        player.Cash -= amount;
+                    }
+                }
+            }
+        }
+    }
+}
